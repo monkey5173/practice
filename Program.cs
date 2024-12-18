1c2bede3b72af9977d2fe2d0c20229065922dc83
monkey5173 <sonseongtae@gmail.com>
2024-12-18 11:38:11 +0900

    [Feat] 기능 추가
    설명

--- Program.cs
+++ Program.cs
@@ -13,6 +13,9 @@ namespace delete
             Console.WriteLine("안뇽하세용");
             Console.WriteLine("테슷트입니다");
             Console.WriteLine("크아앜");
+            Console.WriteLine("기능개발 중");
+            Console.WriteLine("막 반복문도 적고, 클래스도 나누고");
+            Console.WriteLine("클래스 쪼개고, 기능 추가하고");
         }
     }
 }
